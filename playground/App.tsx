651cbcd47d0ccf33e6a321df2cd60cc353b3a5c4
sivchari <shibuuuu5@gmail.com>
2022-09-05 00:52:32 +0900

    Swich ts (#8)
    * switch ts
    
    * add husky
    
    * feat: add ci

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import init, { exec_query } from './wasm';
 
-function App() {
+const App: React.FC = () => {
   const [rows, setRows] = useState('');
   const [query, setQuery] = useState('');
-  const execQuery = (q) => {
+  const execQuery = (q: string) => {
     init().then(() => {
-        setRows(exec_query(q));
+      setRows(exec_query(q));
     })
   }
   return (
     <div className="App">
       <h1> Snowflake Playground </h1>
       <h2> Query </h2>
-      <input type="text" name="query" onChange={(e) => setQuery(e.target.value)} />
+      <input type="text" name="query" onChange={(e: ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)} />
       <button onClick={() => execQuery(query)}> Execute Query </button>
-      <h1> `Result {rows}` </h1>
+      <h1> Result {rows} </h1>
     </div>
   );
 }
---
+++ playground/index.tsx
@@ -0,0 +1,5 @@
+import App from './App';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
