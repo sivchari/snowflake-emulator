dcd8956be1e8e9004f41cc46f09ce21f1a599793
sivchari <shibuuuu5@gmail.com>
2022-10-24 00:24:11 +0900

    Add sqlite (#12)
    * add sqlite3
    
    * delete action

--- server/src/query_request.rs
+++ server/src/query_request.rs
@@ -1,6 +1,11 @@
-use actix_web::{post, web, HttpRequest, HttpResponse};
+use actix_web::{post, web, HttpResponse, Result};
+use futures::TryStreamExt;
 use serde::{Deserialize, Serialize};
 use sf::{parser, plan};
+use sqlx::{Column, Row};
+use std::sync::Arc;
+
+use crate::server::Context;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -32,7 +37,25 @@ struct RowType {
 }
 
 #[post("/queries/v1/query-request")]
-async fn handler(_req: HttpRequest, query_request: web::Json<QueryRequest>) -> HttpResponse {
+async fn handler(
+    ctx: web::Data<Arc<Context>>,
+    query_request: web::Json<QueryRequest>,
+) -> Result<HttpResponse> {
+    // exec sqlite
+
+    // Column Type
+    // `hoge` -> column name
+    // 'hoge' -> string
+    // 1 -> int
+    // 1.1 -> float
+    let mut headers = vec![];
+    let mut rows = sqlx::query("SELECT 1, 1.1, 'a', true").fetch(&ctx.pool.clone());
+    while let Some(row) = rows.try_next().await.unwrap() {
+        headers = row.columns().iter().map(|c| c.name().to_string()).collect();
+        println!("{:?}", headers);
+    }
+
+    // fail: following code
     let stmt = parser::query_to_statment(&query_request.sql_text).unwrap();
     let plan = plan::statement_to_plan(&stmt).unwrap();
     let rows = plan.execute_plan().unwrap();
@@ -60,7 +83,7 @@ async fn handler(_req: HttpRequest, query_request: web::Json<QueryRequest>) -> H
         success: true,
     };
     let body = serde_json::to_string(&exec_response).unwrap();
-    HttpResponse::Ok()
+    Ok(HttpResponse::Ok()
         .content_type("application/json")
-        .body(body)
+        .body(body))
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,14 +1,36 @@
+use actix_web::{web, App, HttpServer};
+use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
+use std::fs::{remove_file, File};
+use std::sync::Arc;
+
 use crate::{auth, query_request};
-use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 pub async fn run(host: &str, port: u16) -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let ctx = Arc::new(Context::new().await);
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(ctx.clone()))
             .service(auth::handler)
             .service(query_request::handler)
     })
     .bind((host, port))?
     .run()
-    .await
+    .await?;
+    remove_file("snowflake-emulator")
+}
+
+pub struct Context {
+    pub pool: SqlitePool,
+}
+
+impl Context {
+    pub async fn new() -> Self {
+        let _ = File::create("snowflake-emulator").unwrap();
+        let pool = SqlitePoolOptions::new()
+            .connect("snowflake-emulator")
+            .await
+            .unwrap();
+        Self { pool }
+    }
 }
